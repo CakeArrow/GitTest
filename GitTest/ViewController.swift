746c6726bf8d7196b1d29cb19ec257e86210d221
CakeArrow <alec.henderson@yahoo.com>
2017-11-06 10:40:50 -0500

    button and stuff

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pointsYall: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
@@ -18,7 +20,12 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.darkGray
     }
 
-
+    @IBAction func buttonPress(_ sender: UIButton) {
+    
+    
+    
+    }
+    
 
 }
 

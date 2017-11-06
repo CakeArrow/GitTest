0dbede6ecf755198782f8d022c716aaa7ee5f84b
CakeArrow <alec.henderson@yahoo.com>
2017-11-06 10:32:26 -0500

    color

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
+        
+        view.backgroundColor = UIColor.darkGray
     }
 
     override func didReceiveMemoryWarning() {
